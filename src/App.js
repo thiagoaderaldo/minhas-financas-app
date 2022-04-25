d7cbec83105b156e709c9d60a5c56f49f35edbd8
thiagoaderaldo <thiagoaderaldo@gmail.com>
2022-04-25 17:29:27 -0300

    Aula 105 - Instalação e teste do bootswatch

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
 import React from 'react';
 
+import 'bootswatch/dist/flatly/bootstrap.css'
 class App extends React.Component{
-
-  state = {
-    numero1: null,
-    numero2: null,
-    resultado: null
-  }
-
-  somar = () =>{
-    const resultado = parseInt(this.state.numero1) + parseInt(this.state.numero2)
-    this.setState({resultado: resultado})
-  }
-  
   render(){
     return(
       <div>
-        <label>Primeiro número: </label>
-        <input  type="text" value={this.state.numero1}
-                onChange={(e) => this.setState({numero1: e.target.value})} />
-        <br/>
-        <label>Segundo número: </label>
-        <input  type="text" value={this.state.numero2}
-                onChange={(e) => this.setState({numero2: e.target.value})} />
-        <br/>
-        <button onClick={this.somar}>Somar</button>
-        <br/>
-        O resultado é: {this.state.resultado}
+        <button type="button" className="btn btn-primary">Primary</button>
       </div>
     )
-  }
+  } 
 }
 export default App;
