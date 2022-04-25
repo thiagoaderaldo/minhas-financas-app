569305472ff6f210088538a46b59c87680f8e4b6
thiagoaderaldo <thiagoaderaldo@gmail.com>
2022-04-25 16:46:32 -0300

    Aula 104 - estado e comportamento de componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
-}
+class App extends React.Component{
 
+  state = {
+    nome: ''
+  }
+  
+  render(){
+    return(
+      <div>
+        <label>Nome: </label>
+        <input  type="text" value={this.state.nome}
+                onChange={(e) => this.setState({nome: e.target.value})} />
+        <br/>
+        O nome digitado foi: {this.state.nome}
+      </div>
+    )
+  }
+}
 export default App;
