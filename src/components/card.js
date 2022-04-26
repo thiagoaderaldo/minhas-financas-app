f69b8e4a0be88070edac3e756d1020bc7e0e695d
thiagoaderaldo <thiagoaderaldo@gmail.com>
2022-04-26 15:58:56 -0300

    Aula 108 - Componentes do formulário de login

--- src/components/card.js
+++ src/components/card.js
@@ -4,8 +4,8 @@ class Card extends React.Component{
 
     render(){
         return(
-            <div className="card mb-3">
-                <div className="card-header">{this.props.title}</div>
+            <div className="card md-3">
+                <h3 className="card-header">{this.props.title}</h3>
                 <div className="card-body">
                     {this.props.children}
                 </div>
--- src/views/login.js
+++ src/views/login.js
@@ -1,24 +1,46 @@
-import React from 'react'; 
-import Card from '../components/card';
+import React from "react";
+import Card from "../components/card";
+import FormGroup from "../components/form-group";
 
-class Login extends React.Component{
-    
-    render(){
-
-        return(
-            <div className="container">
+class Login extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div
+            className="col-md-6"
+            style={{ position: "relative", left: "300px" }}
+          >
+            <div className="bs-docs-section">
+              <Card title="Login">
                 <div className="row">
-                    <div className="col-md-6" style={ {position: 'relative', left: '300px'} }>
-                    <div className="bs-docs-section">
-                        <Card title="Login">
-                            Meu cartão
-                        </Card>
+                  <div className="col-lg-12">
+                    <div className="bs-component">
+                        <fieldset>
+                            <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
+                            <input  type="email" 
+                                    className="form-control" 
+                                    id="exampleInputEmail1" 
+                                    aria-describedby="emailHelp" 
+                                    placeholder="Digite o e-mail" />
+                            </FormGroup>
+                            <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
+                            <input  type="password" 
+                                    className="form-control" 
+                                    id="exampleInputPassword1" 
+                                    placeholder="Password" />
+                            </FormGroup>
+                        </fieldset>
                     </div>
-                    </div>
+                  </div>
                 </div>
+              </Card>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Login
+export default Login;
