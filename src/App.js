0e95aff1976f4d4370c38652c82ce05a5995d911
thiagoaderaldo <thiagoaderaldo@gmail.com>
2022-04-26 15:21:00 -0300

    Aula 107 - Criação dos componentes login e card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import 'bootswatch/dist/flatly/bootstrap.css'
+
+import Login from './views/login';
+
 class App extends React.Component{
   render(){
     return(
       <div>
-        <button type="button" className="btn btn-primary">Primary</button>
+        <Login />
       </div>
     )
   } 
