0ab677ca48746074c182824db34fb882efe32479
Suporte <thiagoaderaldo@gmail.com>
2022-05-04 11:21:47 -0300

    Aula 110 - Centralizaçao do componente login no centro da pagina

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import 'bootswatch/dist/flatly/bootstrap.css'
 
 import Login from './views/login';
+import './custom.css'
 
 class App extends React.Component{
   render(){
