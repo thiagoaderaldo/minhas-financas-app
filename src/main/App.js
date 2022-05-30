0e3ef7d459e7bab2da7e2d423b441fc5f770617e
Thiago Aderaldo Lessa <thiagoaderaldo@gmail.com>
2022-05-30 15:38:01 -0300

    Aula 115 - Criação do componente navbar

--- src/main/App.js
+++ src/main/App.js
@@ -1,14 +1,18 @@
 import "bootswatch/dist/flatly/bootstrap.css";
 import React from "react";
+import Navbar from "../components/navbar";
 import "../custom.css";
 import Rotas from "./rotas";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="container">
-        <Rotas />
-      </div>
+      <>
+        <Navbar />
+        <div className="container">
+          <Rotas />
+        </div>
+      </>
     );
   }
 }
