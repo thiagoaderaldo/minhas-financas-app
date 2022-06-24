84aa2f86433e1976577d00ec781c554976a33175
thiago.lessa <thiago.lessa@dataprev.gov.br>
2022-06-24 14:01:49 -0300

    Aula 125 - Separando a lógica de acesso às APIs dos componentes

---
+++ src/app/apiservice.js
@@ -0,0 +1,30 @@
+import Axios from "axios";
+
+const httpClient = Axios.create({
+    baseURL: "http://localhost:8080"
+})
+
+class ApiService{
+    
+    constructor(apiURL){
+        this.apiURL = apiURL;
+    }
+
+    post(url, objeto){
+        return httpClient.post(url, objeto);
+    }
+
+    put(url, objeto){
+        return httpClient.post(url, objeto);
+    }
+
+    delete(url){
+        return httpClient.delete(url);
+    }
+
+    get(url){
+        return httpClient.get(url);
+    }
+}
+
+export default ApiService
