b016e001488cae811e6f592838c24169319b3779
thiago.lessa <thiago.lessa@dataprev.gov.br>
2022-06-27 07:45:30 -0300

    Aula 127 - Refatoração do componente Home

--- src/views/home.js
+++ src/views/home.js
@@ -1,22 +1,28 @@
 import React from "react";
+import LocalStorageService from "../app/service/localstorageService";
 
-import Axios from "axios";
+import UsuarioService from "../app/service/usuarioService";
 
 class Home extends React.Component {
   state = {
     saldo: 0,
   };
 
+  constructor() {
+    super();
+    this.usuarioService = new UsuarioService();
+  }
+
   componentDidMount() {
+    
+    const usuarioLogado = LocalStorageService.obterItem("_usuario_logado")
 
-    const usuarioLogadoString = localStorage.getItem("_usuario_logado")
-    const usuarioLogado = JSON.parse(usuarioLogadoString)
-
-    Axios.get(`http://localhost:8080/api/usuarios/${usuarioLogado.id}/saldo`)
-      .then(response => {
+    this.usuarioService
+      .obterSaldoPorUsuario(usuarioLogado.id)
+      .then((response) => {
         this.setState({ saldo: response.data });
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error.response);
       });
   }
--- src/views/login.js
+++ src/views/login.js
@@ -3,6 +3,7 @@ import { withRouter } from "react-router-dom";
 import Card from "../components/card";
 import FormGroup from "../components/form-group";
 import UsuarioService from "../app/service/usuarioService";
+import LocalStorageService from "../app/service/localstorageService";
 
 class Login extends React.Component {
   state = {
@@ -23,7 +24,7 @@ class Login extends React.Component {
         senha: this.state.senha
       })
       .then((response) => {
-        localStorage.setItem("_usuario_logado", JSON.stringify(response.data));
+        LocalStorageService.adicionarItem("_usuario_logado", response.data)
         this.props.history.push("/home");
       })
       .catch((erro) => {
