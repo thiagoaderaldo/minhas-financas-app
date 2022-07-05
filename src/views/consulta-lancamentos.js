fdb9e6400e2c72b81d0d0b4f8e0e89ece336b96c
thiago.lessa <thiago.lessa@dataprev.gov.br>
2022-07-05 10:00:54 -0300

    Aula 133 - Criação do componente SelectMenu (combobox)

--- src/views/consulta-lancamentos.js
+++ src/views/consulta-lancamentos.js
@@ -2,10 +2,28 @@ import React from "react";
 import {withRouter} from "react-router-dom"
 import Card from "../components/card";
 import FormGroup from "../components/form-group";
+import SelectMenu from "../components/selectMenu";
 
 class ConsultaLancamentos extends React.Component{
 
     render(){
+
+        const lista = [
+            {label: "Selecione...", value: ""},
+            {label: "Janeiro", value: 1},
+            {label: "Fevereiro", value: 2},
+            {label: "Março", value: 3},
+            {label: "Abril", value: 4},
+            {label: "Maio", value: 5},
+            {label: "Junho", value: 6},
+            {label: "Julho", value: 7},
+            {label: "Agosto", value: 8},
+            {label: "Setembro", value: 9},
+            {label: "Outubro", value: 10},
+            {label: "Novembro", value: 11},
+            {label: "Dezembro", value: 12}
+        ]
+
         return(
             <Card title="Consultar lançamentos">
                 <div className="row">
@@ -18,6 +36,9 @@ class ConsultaLancamentos extends React.Component{
                                     id="inputAno"
                                     placeholder="Digite o ano"    
                                 />
+                            </FormGroup>
+                            <FormGroup htmlFor="inputMes" label="Mês: ">
+                                <SelectMenu className="form-control" lista={lista} />
                             </FormGroup>
                         </div>
                     </div>
